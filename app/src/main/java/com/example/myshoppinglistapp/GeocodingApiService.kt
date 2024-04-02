3ffcd9f3a63eb3f4a0c02ade6edbfdf88b87524b
singaseong <seonghwanlee@gmail.com>
2024-04-02 14:40:37 +0900

    first commit

---
+++ app/src/main/java/com/example/myshoppinglistapp/GeocodingApiService.kt
@@ -0,0 +1,15 @@
+package com.example.myshoppinglistapp
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface GeocodingApiService {
+
+    @GET("maps/api/geocode/json")
+    suspend fun getAddressFromCoordinates(
+        @Query("latlng") latlng: String,
+        @Query("key") apiKey:String
+    ): GeocodingResponse
+}
+
+
